7d88156b32ee46a63f645e81ba06b2dcfb6d6774
Lucas A. C. Lessa <llessa@godaddy.com>
2023-07-24 13:36:12 -1000

    Exclude AccessiblePrivateMethods

--- finder-internals-only.php
+++ finder-internals-only.php
@@ -2,5 +2,7 @@
 
 return \StubsGenerator\Finder::create()
     ->in(['source/woocommerce/src/Internal'])
+    // finder.php already stubs AccessiblePrivateMethods and stubbing it here would cause a conflict.
+    ->notPath('Traits/AccessiblePrivateMethods.php')
     ->sortByName(true)
 ;
